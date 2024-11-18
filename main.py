f468fa3b88b011ba39c7aff6eec6b078ff0827ca
Ruben <174733720+ruben-23@users.noreply.github.com>
2024-11-18 11:19:31 +0200

    Generare populatie

--- main.py
+++ main.py
@@ -13,3 +13,20 @@ class Individ:
 
        ...
 
+def generare_populatie(nr_indivizi, n):
+
+    cromozomi = set()
+    
+    while(nr_indivizi):
+
+        cromozom = tuple(random.sample(list(range(n)), k=n))
+    
+        if cromozom not in cromozomi:
+            cromozomi.add(cromozom)
+            nr_indivizi -= 1
+
+    return cromozomi
+
+populatie = generare_populatie(10, 4)
+for individ in populatie:
+    print(individ)
