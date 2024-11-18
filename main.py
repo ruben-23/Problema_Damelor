2bdbd4f112712c515b3bda4b85afa228997fd81c
Ruben <174733720+ruben-23@users.noreply.github.com>
2024-11-18 11:10:21 +0200

    creare clasa individ

---
+++ main.py
@@ -0,0 +1,15 @@
+import random 
+
+class Individ:
+
+    cromozom = []
+    fitness = 0
+
+    def __init__(self, cromozom):
+        self.cromozom = cromozom
+        self.calc_fitness()
+
+    def calc_fitness(self):
+
+       ...
+
